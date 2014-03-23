6a7ffeae15b434a54c2868de4ece9836ba3ddef0
na-ta <na999ta@gmail.com>
2014-03-23 17:08:37 +0900

    add setting guard&spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+
 	describe "Home page" do
 
 		it "should have the right title" do
   		visit '/static_pages/home'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  		expect(page).to have_title("#{base_title} | Home")
 		end
 
 		it "should have the content 'Sample App'" do
@@ -19,7 +21,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
   		visit '/static_pages/help'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  		expect(page).to have_title("#{base_title} | Help")
 		end
 
 		it "should have the content 'Help'" do
@@ -32,12 +34,25 @@ describe "Static pages" do
 
 		it "should have the title 'About Us'" do
   		visit '/static_pages/about'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+  		expect(page).to have_title("#{base_title} | About Us")
 		end
 
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
 		end
+	end
+
+	describe "Contact" do
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact")
+		end
+		
+		it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
 	end
 end
