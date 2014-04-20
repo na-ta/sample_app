d2495277c895251f7441a9fc02e285cd894aab12
na-ta <na999ta@gmail.com>
2014-04-21 00:08:23 +0900

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -66,7 +66,29 @@ describe "Authentication" do
 
   				it { should have_title('Sign in') }
   			end
+
+        describe "visiting the following page" do
+          before {visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before {visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
   		end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path)}
+        end
+
+        describe "submitting to the destroy action" do
+          before {delete relationship_path(1)}
+          specify {expect(response).to redirect_to(signin_path)}
+        end
+      end
 
       describe "in the Microposts controller" do
 
@@ -113,8 +135,8 @@ describe "Authentication" do
             it { should_not have_title('Sign up') }
             it { should have_title(full_title('')) }
           end
-  			end
-  		end
+  			end  		
+      end
   	end
 
   	describe "as wrong user" do
